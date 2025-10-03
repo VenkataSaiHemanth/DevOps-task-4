a12b0573a39bca0a421bf805f29e6d77339b997f
VenkataSaiHemanth <hemanth9704696@gmail.com>
2025-10-03 23:19:13 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx_container" {
+  name  = "task4-nginx-container"
+  image = docker_image.nginx.latest
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
